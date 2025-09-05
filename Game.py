b5b9bfb4926ad926977882fae0397783cfac51a1
Win39-28 <edwin.cruz44@upr.edu>
2025-09-04 22:17:27 -0400

    TODO Task 3: Change the Ball Color: Done

--- Game.py
+++ Game.py
@@ -138,7 +138,7 @@ while True:
     screen.fill(bg_color)  # Clear screen with background color
     pygame.draw.rect(screen, light_grey, player)  # Draw player paddle
     # TODO Task 3: Change the Ball Color
-    pygame.draw.ellipse(screen, red , ball)  # Draw ball
+    pygame.draw.ellipse(screen, red , ball)  # Draw ball # Ball color changed red
     player_text = basic_font.render(f'{score}', False, light_grey)  # Render player score
     screen.blit(player_text, (screen_width/2 - 15, 10))  # Display score on screen
 
